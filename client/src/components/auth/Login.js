f8a9ff5feedfa0cf504408565de7487c6b8df318
neel-thakker <neelthakker2001@gmail.com>
2021-06-30 20:41:33 +0530

    Starting Part-3

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -95,4 +95,5 @@ class Login extends Component {
 		);
 	}
 }
+
 export default Login;
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import { combineReducers } from "redux";
+import authReducer from "./authReducer";
+import errorReducer from "./errorReducer";
+
+export default combineReducers({
+	auth: authReducer,
+	errors: errorReducer,
+});
+
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
